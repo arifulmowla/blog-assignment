f2a719ddcad39f71032e392d9e4efb1dfe3e0cce
nokibrokes <mawlaariful@gmail.com>
2020-09-06 17:48:15 +0600

    upate build

---
+++ build/precache-manifest.0af00ad08a1dac16512bd8c6fa0f1a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d116b17565c441f4b2a343ae5f0619e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760e8536c97b7c6b13e6",
+    "url": "/static/css/main.e3c30b83.chunk.css"
+  },
+  {
+    "revision": "c925b4d7bd290f726e6e",
+    "url": "/static/js/2.b9de53f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b9de53f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760e8536c97b7c6b13e6",
+    "url": "/static/js/main.92eeb47e.chunk.js"
+  },
+  {
+    "revision": "2b673eaac4d835d3eb99",
+    "url": "/static/js/runtime-main.087926fd.js"
+  }
+]);
